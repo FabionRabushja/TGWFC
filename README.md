081b6f645188a774b9947de5cea1ea68f733865a
Fabion Rabushja <f.rabushja@hastega.it>
2020-04-23 19:07:28 +0200

    Updated name

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Cards Against Humanity
+# The Game With Funny Cards
+
+## Setup
+
+### Dependency install: 
+```
+   npm install
+```
+### Local dev server
+
+To start the local dev server run
+```
+   ng serve
+```
+### Deploy staging: ng run build
+To build the dist for staging env
+```
+   ng run build
+```
+To build the dist for prod env
+```
+   ng run build
+```
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-starter-kit'),
+      dir: require('path').join(__dirname, './coverage/tgwfc'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
